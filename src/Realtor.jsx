fed3508ea1e2e71d7bed9824e92dc5bbf6dcc9aa
Juberson Jules <julesart913@gmail.com>
2024-01-06 16:52:20 -0400

    Agregado de React Router Dom

--- src/Realtor.jsx
+++ src/Realtor.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+
 
 export const Realtor = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path='/' element={ <Home/> } />
+        <Route path='/profile' element={ <Profile/> } />
+        <Route path='/signin' element={ <SignIn/> } />
+        <Route path='/' element={ <Home/> } />
+        <Route path='/' element={ <Home/> } />
+        <Route path='/' element={ <Home/> } />
+
+      </Routes>
+    </Router>
   )
 }
