507734c742c21b181b5b3e1c15a6305e00c09347
Juberson Jules <julesart913@gmail.com>
2024-01-06 17:22:23 -0400

    Ingresar todas las rutas

--- src/Realtor.jsx
+++ src/Realtor.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import ForgotPassword from './pages/ForgotPassword';
-import Offers from './pages/Offers';
+import { Home } from './pages/Home';
+import { Profile } from './pages/Profile';
+import { SignIn } from './pages/SignIn';
+import { SignUp } from './pages/SignUp';
+import { ForgotPassword } from './pages/ForgotPassword';
+import { Offers } from './pages/Offers';
 
 
 export const Realtor = () => {
   return (
     <Router>
       <Routes>
+
         <Route path='/' element={ <Home/> } />
         <Route path='/profile' element={ <Profile/> } />
-        <Route path='/signin' element={ <SignIn/> } />
-        <Route path='/' element={ <Home/> } />
-        <Route path='/' element={ <Home/> } />
-        <Route path='/' element={ <Home/> } />
+        <Route path='/sign-in' element={ <SignIn/> } />
+        <Route path='/sign-Up' element={ <SignUp/> } />
+        <Route path='/forgot-password' element={ <ForgotPassword/> } />
+        <Route path='/offers' element={ <Offers/> } />
 
       </Routes>
     </Router>
